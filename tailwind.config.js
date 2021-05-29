0b15c5763cffa39572248da33eb17f40de1132e1
axax4 <ax4xtq3dq25b@gmail.com>
2021-05-30 05:00:35 +0900

    build:css2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./src/**/*.ts', './src/**/*.tsx'],
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.tsx']
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
